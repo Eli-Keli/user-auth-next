3809a654e936325fd50ccd87fcab90c5a0ed597e
Eli-Keli <muthokaelikeli@gmail.com>
2024-12-18 21:28:29 +0300

    Add middleware for authentication and redirect handling

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname // Get the path from the request URL
+
+    // Ckeck for public paths
+    const isPublicPath = path === '/login' || path === '/signup'
+
+    // get token from cookies
+    const token = request.cookies.get('token')?.value || ''
+
+    // handle redirects
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+    }
+}
+
+// Matching paths: /, /profile, /login, /signup
+export const config = {
+    matcher: [
+        '/',
+        '/profile',
+        '/login',
+        '/signup',
+    ],
+}
