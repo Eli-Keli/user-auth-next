2387a4ff64ed2faf9d3280bc3ebc2fcc4eeafc3e
Eli-Keli <muthokaelikeli@gmail.com>
2024-12-20 20:05:24 +0300

    Add me route to retrieve user information by token

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import axios from "axios";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import toast from "react-hot-toast";
 
 export default function ProfilePage() {
     const router = useRouter();
+    const [data, setData] = useState("nothing");
 
     const logout = async () => {
         try {
@@ -18,6 +21,12 @@ export default function ProfilePage() {
         }
     }
 
+    const getUserDetails = async () => {
+        const response = await axios.get("/api/users/me");
+        console.log(response.data);
+        setData(response.data.data._id);
+    }
+
     return (
         <div className='flex flex-col justify-center items-center h-screen'>
             <h1 className='text-2xl text-center'>Profile Page</h1>
@@ -25,11 +34,20 @@ export default function ProfilePage() {
             className='w-1/4 my-4'
             />
             <h2 className='text-xl'>Welcome to your profile page</h2>
+
+            <p className='text-lg mt-4 bg-orange-400'>User ID: {data === "nothing" ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link> }</p>
+
             <button 
             onClick={logout}
             className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'
             >
                 Logout
+            </button>
+            <button
+            onClick={getUserDetails}
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4"
+            >
+                Get User Details
             </button>
         </div>
     )
